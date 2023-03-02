bf9c5b25f50e87205da279ed98b2cb03dffba44a
CAIOKILLER <114203977+CAIOKILLER@users.noreply.github.com>
2023-03-02 14:49:24 -0300

    Add files via upload

---
+++ TextFieldTest/src/textfieldtest/TextFieldTest.java
@@ -0,0 +1,16 @@
+package textfieldtest;
+
+import javax.swing.JFrame;
+
+public class TextFieldTest {
+
+    public static void main(String[] args) {
+
+    TextFieldFrame textFieldFrame = new TextFieldFrame();
+    textFieldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    textFieldFrame.setSize(350, 100);
+    textFieldFrame.setVisible(true);
+
+    }
+
+}
