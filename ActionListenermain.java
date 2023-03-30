142d81671f84afe04b51f349d3aaf163645e9e83
CAIOKILLER <114203977+CAIOKILLER@users.noreply.github.com>
2023-03-30 16:28:30 -0300

    Create ActionListenermain.java

---
+++ ActionListenermain.java
@@ -0,0 +1,13 @@
+package flowlayoutframe;
+
+
+import javax.swing.JFrame;
+
+public class FlowLayoutDemo {
+    public static void main (String[] args){
+        FlowLayoutFrame flowLayoutFrame = new  FlowLayoutFrame();
+        flowLayoutFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        flowLayoutFrame.setSize(300, 75);
+        flowLayoutFrame.setVisible(true);
+    }
+}
